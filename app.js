7156a55c0719b58fc0f3b91988921d9c6e4bcc45
zychzygaz <78308744+zychzygaz@users.noreply.github.com>
2021-03-30 11:30:06 +0200

    commit no. ?

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const inputs = document.querySelectorAll("input");
 const smalls = document.querySelectorAll("small");
 const form = document.querySelector("form");
+const name = document.getElementById("name");
+const zipCode = document.getElementById("zipCode");
+const email = document.getElementById("email");
+const phoneNumber = document.getElementById("phoneNumber");
 
-console.log(smalls);
+console.log(name, zipCode, email, phoneNumber);
+
+// console.log(smalls);
 form.addEventListener("submit", function (a) {
     a.preventDefault();
+    // if (name.value !== "") {
+    //     name.className = "success";
+    // }
+    // if (zipCode.value !== "") {
+    //     zipCode.className = "success";
+    // }
+    // if (email.value !== "") {
+    //     email.className = "success";
+    // } else if (!isEmail(email.value)) {
+    //     email.parentElement.querySelector("small").className = "errorSmall";
+    // }
 
-    function checker(bor) {
-        if (bor.value === "") {
-            bor.className = "error";
-            small.forEach((e) => {
-                e.className = "smallError";
-            });
+    // if (phoneNumber !== "") {
+    //     phoneNumber.className = "success";
+    // }
+
+    // Does the same things as above
+
+    inputs.forEach((e) => {
+        if (e.value.trim() === "") {
+            e.parentElement.querySelector("small").className = "errorSmall";
+        } else {
+            e.className = "success";
         }
-    }
+    });
+
     // Go back to the default after 1,5 sek
     setTimeout(function () {
         inputs.forEach((e) => {
@@ -22,5 +45,7 @@ form.addEventListener("submit", function (a) {
                 s.className = "";
             });
         });
-    }, 1500);
+    }, 2500);
 });
+
+// name.className = "success";
